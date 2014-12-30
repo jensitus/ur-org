d167561f5a60075abe30677b0ce63d0d85a64996
jens <jens@service-b.org>
2014-12-30 08:45:10 +0100

    paginate

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 5)
     end
   end
 
