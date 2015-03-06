4d7ea2849c473ed429ebfb2c54f6c1cc3dc0d1ee
jens <jens@service-b.org>
2015-03-06 08:44:29 +0100

    the follow bug is fixed now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,14 @@ class UsersController < ApplicationController
   def following
     @title = 'I am connected with:'
     @user = User.friendly.find(params[:id])
-    @users = @user.following.paginate(page: params[:page], :per_page => 5 )
+    @users = @user.following.page(params[:page]).per(5)
     render 'show_follow'
   end
 
   def followers
     @title = 'They connected me:'
     @user = User.friendly.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.page(params[:page]).per(5)
     render 'show_follow'
   end
 
