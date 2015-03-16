0ee17194f19c46ad5c0d1b5c5f828a12139db893
jens <jens@service-b.org>
2015-03-16 08:26:33 +0100

    no conversations on startpage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,7 @@ class StaticPagesController < ApplicationController
       fresh_when :etag => [@feed_items, current_user]
     else
       @static_page_image = Micropost.where('picture IS NOT NULL').sample(1)[0].picture.url
+
       # redirect_to new_user_session_url
     end
   end
