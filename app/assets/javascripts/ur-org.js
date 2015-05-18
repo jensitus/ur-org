563d5a1d2ddb29481dcc5a052af6e0da953af92f
jens <jens@service-b.org>
2015-05-18 13:40:38 +0200

    mist

--- app/assets/javascripts/ur-org.js
+++ app/assets/javascripts/ur-org.js
@@ -26,3 +26,19 @@ $(document).ready(function() {
  * * * * * * * * * * * * * * * */
 
 UnobtrusiveFlash.flashOptions['timeout'] = 4000;
+
+
+/*******************************
+ *  the form check:
+ *******************************/
+
+function checkform() {
+    var f = document.forms["the_comment_form"].elements;
+    var cansubmit = true;
+    for (var i = 0; i < f.length; i++) {
+        if ("value" in f[i] && f[i].value.length == 0) {
+            cansubmit = false;
+        }
+    }
+    document.getElementById("submit").disabled = !cansubmit;
+}
