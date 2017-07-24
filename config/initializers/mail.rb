b9a16060a86529d2a01d6b64743aeca143639fe0
jens <jens@service-b.org>
2017-07-24 20:41:10 +0200

    mail user is secret

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -11,7 +11,7 @@ ActionMailer::Base.smtp_settings = {
     address: 'smtprelaypool.ispgateway.de',
     port: 587,
     domain: 'ist-ur.org',
-    user_name: 'info@ist-ur.org',
+    user_name: Rails.application.secrets.mail_user,
     password: Rails.application.secrets.mail_pw,
     authentication: :plain,
     enable_starttls_auto: true
