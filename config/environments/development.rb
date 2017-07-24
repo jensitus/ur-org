0abf6f81ec13879739ceb0701e6e5249d44bb74b
jens <jens@service-b.org>
2017-07-24 20:43:28 +0200

    no smtp in developmentrb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,15 +17,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener #:smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.smtp_settings = {
-  #     address: 'posteo.de',
-  #     port: 587,
-  #     domain: 'ist-ur.org',
-  #     user_name: 'info-ist-ur-org@posteo.de',
-  #     password: '*****',
-  #     authentication: :plain,
-  #     enable_starttls_auto: true
-  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
