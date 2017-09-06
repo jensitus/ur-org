334b6ea40e249d22ae7157b6595694de240bb0cb
jens <jens@ist-ur.org>
2017-09-06 21:41:33 +0200

    comment, notice and activity-tracking will be also deleted after deleting a photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,23 @@ class Photo < ActiveRecord::Base
   belongs_to :micropost
   mount_uploader :picture, PictureUploader
   belongs_to :photo_gallery
+
   has_and_belongs_to_many :notices
-  has_many :photo_comments
+  has_many :photo_comments, dependent: :destroy
   has_many :comments, through: :photo_comments
+
+  before_destroy :delete_notice
+
+  private
+
+  def delete_notice
+    puts 'delete the notice: ' + self.inspect
+    n = self.notices
+    n.each do |notice|
+      activity = PublicActivity::Activity.find_by(trackable_id: notice.id)
+      activity.delete
+      notice.delete
+    end
+  end
+
 end
