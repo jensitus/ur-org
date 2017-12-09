f14d037ae6c3758fcc7ef9a20d6e720316d1217f
jens <jens@ist-ur.org>
2017-12-09 11:20:16 +0100

    check_revision

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,6 +70,24 @@ end
 
 namespace :deploy do
 
+  desc 'Make sure local git is in sync with remote'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts 'WARNING: HEAD is not the same as origin/master'
+        puts "Run `git push` to sync changes"
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
