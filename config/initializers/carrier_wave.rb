f827782de3d8fe843112865b80a58be20943ed9b
jens <jens@ist-ur.org>
2017-12-10 23:21:04 +0100

    so eine verfickte scheiße diese verdammten aws mist scheiß dinger

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@
         # Configuration for Amazon AWS
         :provider => 'AWS',
         :region => 'eu-central-1',
-        :aws_access_key_id => Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
-        :aws_secret_access_key => Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
+        :aws_access_key_id => ENV['S3_ACCESS_KEY'], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
+        :aws_secret_access_key => ENV['S3_SECRECT_KEY'] # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
     }
-    config.fog_directory = Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
+    config.fog_directory = ENV['S3_BUCKET'] # Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
   end
 # end
