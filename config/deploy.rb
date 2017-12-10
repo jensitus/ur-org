330f4efc418f97092ec01a217c5a379fe7f8513e
jens <jens@ist-ur.org>
2017-12-10 19:15:41 +0100

    now the deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,81 +1,59 @@
-require 'pg_search'
-# config valid only for current version of Capistrano
-lock '3.10.1'
+# Change these
+server '84.39.96.50', port: 22, roles: [:web, :app, :db], primary: true
 
-set :application, 'receta' # 'my_app_name'
-set :repo_url, 'https://github.com/jensitus/ur-org.git'
+set :repo_url,        'git@github.com:jensitus/ur-org.git'
+set :application,     'receta'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/jens/receta'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-set :use_sudo, true
-
-# Default value for :log_level is :debug
-set :log_level, :debug
-
-# Default value for :pty is false
-set :pty, false
-
-set :puma_bind, "unix:///home/jens/receta/shared/tmp/sockets/receta-puma.sock"
-set :puma_state, "home/jens/receta/shared/tmp/pids/puma.state"
-set :puma_pid, "home/jens/receta/shared/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.error.log"
-# set :puma_error_log, "#{release_path}/log/puma.puma.access.log"
-# set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/database.yml} #fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system} # fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-#set :default_env, { path: '~/.rvm/bin/rvm' }  # { path: "/opt/ruby/bin:$PATH" }
-#set :stage, :production
-#set :rails_env, 'production'
-
-# Default value for keep_releases is 5
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
 # set :keep_releases, 5
 
-# # # # # # # # # # # # # # # #
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir home/jens/receta/shared/tmp/sockets -p"
-      execute "mkdir home/jens/receta/shared/tmp/pids -p"
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
 
   before :start, :make_dirs
 end
-# # # # # # # # # # # # # # # #
 
 namespace :deploy do
-
-  desc 'Make sure local git is in sync with remote'
+  desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts 'WARNING: HEAD is not the same as origin/master'
-        puts "Run `git push` to sync changes"
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
       end
     end
   end
@@ -91,73 +69,16 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # execute :touch, release_path.join('tmp/restart.txt')
       invoke 'puma:restart'
     end
   end
-  # after :restart, :clear_cache do
-  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #     # Here we can do anything such as:
-  #     # within release_path do
-  #     #   execute :rake, 'cache:clear'
-  #     # end
-  #   end
-  # end
-  before :starting,  :check_revision
-  after  :finishing, :compile_assets
-  after  :finishing, :cleanup
-  after  :finishing, :restart
 
-  # after :publishing, 'deploy:restart'
-  # after :finishing, 'deploy:cleanup'
-
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
 
-# the sidekiq
-# namespace :sidekiq do
-#   task :quiet do
-#     on roles(:app) do
-#       # Horrible hack to get PID without having to use terrible PID files
-#       puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
-#     end
-#   end
-#   task :restart do
-#     on roles(:app) do
-#       execute :sudo, :initctl, :restart, :workers
-#     end
-#   end
-# end
-#
-# after 'deploy:starting', 'sidekiq:quiet'
-# after 'deploy:reverted', 'sidekiq:restart'
-# after 'deploy:published', 'sidekiq:restart'
-
-# namespace :adm do
-#   task :sake do
-#     on roles(:app) do
-#       within "#{current_path}" do
-#         with rails_env: :production do
-#           execute :rake, 'admin:the_user'
-#         end
-#       end
-#     end
-#   end
-# end
-#
-# after 'deploy:published', 'adm:sake'
-
-# the search
-# namespace :pgs do
-#   desc 'Invoke pg_search task'
-#   task :invoke do
-#     on roles(:app) do
-#       within "#{current_path}" do
-#         with rails_env: :production do
-#           execute :rake, 'pg_search:multisearch:rebuild[Micropost]'
-#         end
-#       end
-#     end
-#   end
-# end
-#
-# after 'deploy:published', 'pgs:invoke'
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
