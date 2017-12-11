c91b758c60824f418a40286c6e27dbc0c2dbe717
jens <jens@ist-ur.org>
2017-12-12 00:07:53 +0100

    active job

--- app/jobs/mail_after_comment_job.rb
+++ app/jobs/mail_after_comment_job.rb
@@ -5,7 +5,6 @@ class MailAfterCommentJob < ActiveJob::Base
     # Do something later
     puts "Do send the message later"
     puts comment_join_object
-    puts u_a
     CommentMailer.comment_mail(comment_join_object)
   end
 end
