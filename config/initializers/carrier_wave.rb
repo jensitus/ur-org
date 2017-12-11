8be411e598c865b7c0ef0443cfb1dc5d7fb37fb1
jens <jens@ist-ur.org>
2017-12-11 07:21:37 +0100

    s3 region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp') # adding these...
-    config.cache_dir = 'carrierwave' # ...two lines
+    # config.root = Rails.root.join('tmp') # adding these...
+    # config.cache_dir = 'carrierwave' # ...two lines
     config.fog_credentials = {
         # Configuration for Amazon AWS
         :provider => 'AWS',
-        :region => 'eu-central-1',
-        :s3_access_key_id => ENV['S3_ACCESS_KEY'], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
-        :s3_secret_access_key => ENV['S3_SECRECT_KEY'] # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
+        :aws_access_key_id => ENV['S3_ACCESS_KEY'], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
+        :aws_secret_access_key => ENV['S3_SECRECT_KEY'], # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
+        :region => 'eu-central-1'
     }
     config.fog_directory = ENV['S3_BUCKET'] # Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
   end
