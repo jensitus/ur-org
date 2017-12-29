d006f41211ccf8fd9e6830530740b58a44fb3ece
jens <jens@ist-ur.org>
2017-12-29 16:16:32 +0100

    quotation mark

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,10 +5,10 @@ if Rails.env.production?
     config.fog_credentials = {
         # Configuration for Amazon AWS
         :provider => 'AWS',
-        :aws_access_key_id => ENV['S3_ACCESS_KEY'], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
-        :aws_secret_access_key => ENV['S3_SECRECT_KEY'], # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
+        :aws_access_key_id => ENV["S3_ACCESS_KEY"], # Rails.application.secrets.s3_access_key,      # ,ENV['S3_ACCESS_KEY']
+        :aws_secret_access_key => ENV["S3_SECRECT_KEY"], # Rails.application.secrets.s3_secret_key   # ENV['S3_SECRECT_KEY']
         :region => 'eu-central-1'
     }
-    config.fog_directory = ENV['S3_BUCKET'] # Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
+    config.fog_directory = ENV["S3_BUCKET"] # Rails.application.secrets.s3_bucket              # ENV['S3_BUCKET']
   end
 end
