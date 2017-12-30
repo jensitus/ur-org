d3ce600d4e54a6f92c12e23246baa7baeed7966b
jens <jens@ist-ur.org>
2017-12-30 21:18:47 +0100

    sitekiq_role

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :sidekiq_role, :web
 
 ## Defaults:
 # set :scm,           :git
