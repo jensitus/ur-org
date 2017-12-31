720168086ea0c335e56ac54d7bc629ed7c8c548a
jens <jens@ist-ur.org>
2017-12-31 10:52:11 +0100

    sidekiq pogo

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
+set :pty,             false
 set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
@@ -89,12 +89,13 @@ namespace :sidekiq do
   task :quiet do
     on roles(:app) do
       # Horrible hack to get PID without having to use terrible PID files
-      puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+      # puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP")
     end
   end
   task :restart do
     on roles(:app) do
-      execute :initctl, :restart, :workers
+      execute :sudo, :initctl, :restart, :workers
     end
   end
 end
