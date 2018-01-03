abf37a728bdd87d2e470d9ae1b68d8b56f878b91
jens <jens@ist-ur.org>
2018-01-03 20:48:07 +0100

    fucking redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,8 +3,8 @@ require 'sidekiq'
 Sidekiq::Extensions.enable_delay!
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 3, url: ENV["REDIS_PROVIDER"] }
-  # config.redis = { url: 'redis://localhost:6379' }
+  # config.redis = { size: 3, url: ENV["REDIS_PROVIDER"] }
+  config.redis = { url: 'redis://localhost:6379' }
 end
 
 Sidekiq.configure_server do |config|
