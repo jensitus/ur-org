413a9d041edcebbb9727404e1b4916c8caf7cc98
jens <jens@ist-ur.org>
2018-01-24 23:14:55 +0100

    mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -9,10 +9,10 @@
 # }
 ActionMailer::Base.smtp_settings = {
     address: 'smtprelaypool.ispgateway.de',
-    port: 465,
+    port: 587,
     domain: 'ist-ur.org',
     user_name: ENV['MAIL_USER'], # Rails.application.secrets.mail_user,
     password: ENV['MAIL_PW'], # Rails.application.secrets.mail_pw,
-    authentication: :login,
+    authentication: :plain,
     enable_starttls_auto: true
 }
