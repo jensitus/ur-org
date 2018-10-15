95c030a43611500091baf2cd1c260e8967aca42f
Jens <jens@ist-ur.org>
2018-10-15 21:12:17 +0200

    pty true

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :pty,             false
+set :pty,             true
 set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
