a33bcacce661b29a630de3ba998c15863db01048
Jens <jens@ist-ur.org>
2018-10-15 21:27:28 +0200

    execute without sudo

--- config/deploy.rb
+++ config/deploy.rb
@@ -109,7 +109,7 @@ namespace :sidekiq do
   end
   task :restart do
     on roles(:app) do
-      execute :sudo, :initctl, :restart, :workers
+      execute :initctl, :restart, :workers
     end
   end
 end
