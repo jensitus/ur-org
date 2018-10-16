f6b75af29db3f61901db52df7839100f2f14f374
Jens <jens@ist-ur.org>
2018-10-16 21:34:57 +0200

    :yes_sidekiq-capistrano_gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa_service.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -100,23 +100,23 @@ end
 # kill -s SIGTERM pid   # Stop puma
 #
 # the sidekiq
-namespace :sidekiq do
-  task :quiet do
-    on roles(:app) do
-      # Horrible hack to get PID without having to use terrible PID files
-      puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
-      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP")
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :initctl, :restart, :workers
-    end
-  end
-end
-
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
+# namespace :sidekiq do
+#   task :quiet do
+#     on roles(:app) do
+#       # Horrible hack to get PID without having to use terrible PID files
+#       puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+#       puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP")
+#     end
+#   end
+#   task :restart do
+#     on roles(:app) do
+#       execute :sudo, :initctl, :restart, :workers
+#     end
+#   end
+# end
+#
+# after 'deploy:starting', 'sidekiq:quiet'
+# after 'deploy:reverted', 'sidekiq:restart'
+# after 'deploy:published', 'sidekiq:restart'
 
 # after 'deploy:published', 'custom:appearance'
