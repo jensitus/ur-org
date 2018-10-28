1dc4bf28edf3a74735599194b4582f15ebfb2e6c
Jens <jens@ist-ur.org>
2018-10-28 15:13:43 +0100

    trying to get rid of coffee

--- app/jobs/read_post_job.rb
+++ app/jobs/read_post_job.rb
@@ -8,9 +8,9 @@ class ReadPostJob < ApplicationJob
     if r.read == false && r.user_notified == false && r.entity_type == StaticData::MAILBOXER_CONVERSATION
       ReadPostMailer.inform_about_new_message(r).deliver_now
     elsif r.read == false && r.entity_type == StaticData::MICROPOST
-      logger.debug("it is an unread microposting, but currently we are doing nothing at this point")
+      puts "[info] it is an unread microposting, but currently we are doing nothing at this point"
     else
-      puts "everything is read now"
+      puts "[info] everything is read now"
     end
   end
 
